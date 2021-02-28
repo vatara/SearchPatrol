45c1dff20a0abfd2eb54e584536ebba533ab4aa2
vatara <vatara420@gmail.com>
2021-02-28 16:29:49 -0500

    Remove unused files

--- SearchPatrol.Wpf/MainWindow.xaml.cs
+++ SearchPatrol.Wpf/MainWindow.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using SearchPatrol.Common;
 using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Navigation;
 
 namespace SearchPatrol.Wpf
 {
@@ -51,37 +47,6 @@ namespace SearchPatrol.Wpf
             }
 
             return IntPtr.Zero;
-        }
-
-        private void LinkOnRequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            System.Diagnostics.Process.Start(e.Uri.ToString());
-        }
-
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
-        {
-            string sText = e.Text;
-            foreach (char c in sText)
-            {
-                if ( ! (('0' <= c && c <= '9') || c == '+' || c == '-' || c == ',') )
-                {
-                    e.Handled = true;
-                    break;
-                }
-            }
-        }
-
-        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            if (sender is Slider oSlider && DataContext is SimvarsViewModel oContext)
-            {
-                oContext.SetTickSliderValue((int)oSlider.Value);
-            }
-        }
-
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
     }
 }
