e64fc88f9d1c708d2c2ae98db9d2180304557b97
vatara <vatara420@gmail.com>
2021-03-20 14:28:43 -0400

    Move standalone zip into output folder

--- SearchPatrol.Wpf/Installer/build-standalone.ps1
+++ SearchPatrol.Wpf/Installer/build-standalone.ps1
@@ -1,5 +1,5 @@
 Push-Location $PSScriptRoot
 msbuild .. -p:Configuration=Release /p:Platform=x64
 # compress-archive makes zips that don't work many places because it uses \ instead of / for folders
-.\7z u SearchPatrol-standalone.zip ../bin/x64/Release/*
+.\7z u Output\SearchPatrol-standalone.zip ../bin/x64/Release/*
 Pop-Location
